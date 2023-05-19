d7e00b68a9f9c76cb29dc47db64e5231b2e05506
harsh <harshkhosla9945@gmail.com>
2023-05-19 17:48:47 +0530

    first commit

---
+++ src/components/header/Navbar.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import logo from './jouls.png';
+import "./header.css"
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <nav className="navbar bg-body-tertiary">
+            <div className="container">
+              <a className="navbar-brand" href="#">
+                <img src={logo} alt="Bootstrap" width="100" height="54" />
+              </a>
+            </div>
+          </nav>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active nav-item-text"
+                  aria-current="page"
+                  href="#"
+                  style={{ color: 'green', textAlign: 'center' }}
+                >
+                  About Us
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle nav-item-text"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  style={{ color: 'green', textAlign: 'center' }}
+                >
+                  Survey Page
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item nav-item-text"  style={{ color: 'green', textAlign: 'center' }}href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item nav-item-text" style={{ color: 'green', textAlign: 'center' }} href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item nav-item-text" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active nav-item-text"
+                  aria-current="page"
+                  href="#"
+                  style={{ color: 'green', textAlign: 'center' }}
+                >
+                  Blogs
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active nav-item-text"
+                  aria-current="page"
+                  href="#"
+                  style={{ color: 'green', textAlign: 'center' }}
+                >
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              {/* <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              /> */}
+              <button className="btn btn-outline-success" type="submit">
+                Download App
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
